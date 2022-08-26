6b4fd8e3914e813babaa2d62d06398f4680984c2
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-08-26 21:24:56 +0300

    Klasörleri karşılaştırma işlemleri

--- modules/dir_sync/syncer.py
+++ modules/dir_sync/syncer.py
@@ -17,6 +17,17 @@ import time
 from typing import Union
 from pathlib import Path
 from .version import __pkg_name__
+
+
+class DCMP:
+    """Dizinleri kıyaslarken verileri saklamak için kullanılan sahte obje"""
+    def __init__(self, left, right, common, db=None):
+        self.left_only: set = left  # kaynak veya geçmiş
+        self.right_only: set = right  # hedef veya şu an
+        self.common: set = common  # ortak
+        self.db: Union[dict, None] = db
+
+
 class Syncer:
     """ Dizinleri senkronize etme sınıfı """
 
@@ -73,7 +84,110 @@ class Syncer:
             return None
 
         # All right!
+        self.sync()
         self._endtime = time.time()
+
+    def sync(self):
+
+        self.log(f'Dizinler senkronize ediliyor:'
+                 f' "{self._pc_path}" & "{self._drive_path}"')
+
+        # kaynak pc dizini taraması yapılır
+        pc_paths, self._cmp_pc = self._self_compare(self._pc_path)
+        # hedef drive dizin taraması yapılır
+        drive_paths, self._cmp_drive = self._self_compare(self._drive_path)
+        # iki dizin arasındaki farklar alınır
+        self._dcmp = self._compare(pc_paths, drive_paths)
+    @staticmethod
+    def _compare(pc_paths: set, drive_paths: set) -> DCMP:
+        """
+        Dizinlerin dosya ve klasör yollarını alır, farklılıkları döndürür
+
+        :param pc_paths: kaynak dizin yolu
+        :type pc_paths: set
+        :param drive_paths: hedef dizin yolu
+        :type drive_paths: set
+        :return: farklılıklar
+        :rtype: DCMP
+        """
+
+        common = pc_paths.intersection(drive_paths)
+        pc_paths.difference_update(common)
+        drive_paths.difference_update(common)
+
+        return DCMP(pc_paths, drive_paths, common)
+
+    def _self_compare(self, comp_dir: Path) -> tuple:
+        """
+        Dizini kendi içinde tarama yapar ve dosyalar ile farklılıkları döndürür
+
+        :param comp_dir: dizin yolu
+        :type comp_dir: str
+        :return: dosyalar ile farklılıklar (set, DCMP)
+        :rtype: tuple
+        """
+
+        self.log(f'Taranıyor: {comp_dir}')
+        self._numdirs += 1
+
+        # önceki tarama sonucu alınır
+        db = self._read_db(comp_dir)
+        last_paths = set(db.keys())
+
+        # dizin gezilir
+        paths = set(self._walk_in_dir(comp_dir).keys())
+        new_paths = paths.copy()
+
+        # önceki hali ile ortak dosyalar ve farklı dosyalar belirlenir
+        common = paths.intersection(last_paths)
+        new_paths.difference_update(common)
+        last_paths.difference_update(common)
+
+        return paths, DCMP(last_paths, new_paths, common, db)
+
+    def _walk_in_dir(self, comp_dir: Path, counting: bool = True) -> dict:
+        """
+        Dizin içinde gezer ve dosyaları/klasöri döndürür
+
+        :param comp_dir: dizin yolu
+        :type comp_dir: str
+
+        :return: dosya ve klasörlerin yolları
+        :rtype: set
+        """
+
+        paths = {}
+        for cwd, dirs, files in os.walk(comp_dir):
+            self._numdirs += len(dirs) if counting else 0
+            for f in dirs + files:
+                if f == self._db_file:
+                    continue
+
+                full_path = Path(cwd).joinpath(f)
+                re_path = str(full_path.relative_to(comp_dir))
+                paths[re_path] = self.get_file_info(full_path)
+
+        return paths
+
+    @staticmethod
+    def get_file_info(full_path) -> dict:
+        info = {}
+
+        # klasör mü?
+        is_dir = full_path.is_dir()
+        info["is_dir"] = is_dir
+
+        # eğer klasör değilse dosya bilgilerini ekle
+        if not is_dir:
+            info["file_name"] = full_path.stem
+            info["file_extension"] = full_path.suffix
+
+        # dosya klasör zaman bilgilerini alalım
+        path_stat = full_path.stat()
+        info["updated_at"] = path_stat.st_mtime
+        info["created_at"] = path_stat.st_ctime
+
+        return info
 
     def report(self):
         """ Print report of work at the end """
